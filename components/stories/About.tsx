3a156f2c2c6dc6d93f07833a4b15f1ff207027a3
Antonis Valamontes <tony@geopay.me>
2022-07-01 15:14:34 +0300

    Update About.tsx

--- components/stories/About.tsx
+++ components/stories/About.tsx
@@ -22,7 +22,7 @@ const About: FunctionComponent = () => {
       )}
     >
       <div>
-        <iframe style={{ aspectRatio: '560 / 315', width: '100%' }} src="https://www.youtube-nocookie.com/embed/ysuZ_SW2bK0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        <iframe style={{ aspectRatio: '560 / 315', width: '100%' }} src="https://www.youtube-nocookie.com/embed/sEb32onUzDs" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
       </div>
     </AboutContainer>
   )
