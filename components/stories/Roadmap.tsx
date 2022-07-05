2c46fb3c2b6f0bfe6d22e8861bffd123c7d44553
Antonis Valamontes <tony@geopay.me>
2022-07-05 21:42:01 +0300

    Update Roadmap.tsx

--- components/stories/Roadmap.tsx
+++ components/stories/Roadmap.tsx
@@ -5,7 +5,7 @@ const Roadmap: FunctionComponent = () => {
   return (
     <TimelineContainer id="roadmap" title="Roadmap" items={[
       {
-        image: 'https://lh3.googleusercontent.com/qDLIwHJ5_bAoRUtlSGZuM3jTU_KNTvoLvDQSH8vv6ReuuxNIXCmgUq37EprQduZr2_XJhFHSLCxwm2XD6MTD2TIR-o4kX7U5QQ2148I=w330',
+        image: '',
         title: ' Early Release  - All Sales',
         content: (<>
           &#183; Whitelist for Gold NFT Certificate Giveway<br />
