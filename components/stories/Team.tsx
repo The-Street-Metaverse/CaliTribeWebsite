61c6216193295d53c84a1d4c47ffc3a12e252768
Antonis Valamontes <tony@geopay.me>
2022-07-07 17:20:46 +0300

    Update Team.tsx

--- components/stories/Team.tsx
+++ components/stories/Team.tsx
@@ -8,7 +8,7 @@ const Team: FunctionComponent = () => {
       title="Meet the Cultivar Team"
       items={[
         {
-          image: 'https://lh3.googleusercontent.com/cuj35f7TVERNS-ypwEWON3wA4CRT9rrwnEWB3vdAC3OnR7TByv3yrFyZyvpMicsC2EdYtrHawXhEzoCWwqzDNBsV24SBGLb1kDgaxA=w600',
+          image: 'https://raw.githubusercontent.com/The-Street-Metaverse/CaliTribeWebsite/f80be2ddc9dd192771ee83cea3022e6f3dda3114/images/RastaFrog.svg',
           title: 'D"Artagnan',
           role: 'Dev/Techie',
           social: [
@@ -17,7 +17,7 @@ const Team: FunctionComponent = () => {
           ]
         },
         {
-          image: 'https://lh3.googleusercontent.com/kg-A4af5pIfGfb2qQ3YuRHL97g98ziQVUFuAbanyrQltEr9Kzs3rJOub7rr2gacUuy9PgeD1lu24p_H7tWZjK1xKp-srbKFHuEpN=w600',
+          image: 'https://raw.githubusercontent.com/The-Street-Metaverse/CaliTribeWebsite/f80be2ddc9dd192771ee83cea3022e6f3dda3114/images/RastaFrog.svg',
           title: 'Pavvy',
           role: 'Artist/Animator',
           social: [
@@ -27,7 +27,7 @@ const Team: FunctionComponent = () => {
           ]
         },
         {
-          image: 'https://lh3.googleusercontent.com/90MaVfN87DGP3I_c7CK0-S-jWLA8CTEja1xxFKtAJaeI2t5z9z5F9g4Ph6ZJsXVCVlnMfWjKJQxrycS6QIPxUuWp4hnU3ptUWggx6IM=w600',
+          image: 'https://raw.githubusercontent.com/The-Street-Metaverse/CaliTribeWebsite/f80be2ddc9dd192771ee83cea3022e6f3dda3114/images/RastaFrog.svg',
           title: 'Alya',
           role: 'Creative Director',
           social: [
@@ -36,7 +36,7 @@ const Team: FunctionComponent = () => {
           ]
         },
         {
-          image: 'https://lh3.googleusercontent.com/DEwYW2YzgdNQeNWzDnexr0kylmo-NZ2AI75_d6piM9lxx8YnzUz7vC5zs5_GO_XzbRAkbUUh5mGLRFWWoew4sw1r-SwR1UmAxfF0zQ=w600',
+          image: 'https://raw.githubusercontent.com/The-Street-Metaverse/CaliTribeWebsite/f80be2ddc9dd192771ee83cea3022e6f3dda3114/images/RastaFrog.svg',
           title: 'Lawana',
           role: 'Marketeer',
           social: [
